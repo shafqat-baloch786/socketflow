67bad75072d0e8ab5f558f5ed7d25fed1e57278d
Shafqat Baloch <shafqat-baloch786@users.noreply.github.com>
2025-12-23 20:54:18 +0500

    feat(profile): add API endpoint for updating user profile

--- api/controllers/profile_controller.js
+++ api/controllers/profile_controller.js
@@ -1,9 +1,11 @@
 const catchAsync = require('../utils/catch_async');
+const User = require('../models/User');
+const mongoose = require('mongoose');
 
 // Get profile data
 const getProfile = catchAsync(async (req, res) => {
     const user = req.user;
-    
+
     if (!user) {
         return res.status(404).json({
             success: false,
@@ -25,6 +27,58 @@ const getProfile = catchAsync(async (req, res) => {
     });
 });
 
+
+// Editing/updating profile
+const editProfile = catchAsync(async (req, res, next) => {
+
+    const id = req.user._id;
+    const allowedFields = ['name', 'email', 'avatar'];
+    const updates = {};
+
+    allowedFields.forEach(field => {
+        if (req.body[field] !== undefined) {
+            updates[field] = req.body[field];
+        }
+    });
+
+    // Check if updates is empty or not
+    if (Object.keys(updates).length === 0) {
+        return res.status(400).json({
+            message: "No valid field provided!"
+        });
+    }
+
+    // Find user by ID and update
+    const user = await User.findByIdAndUpdate(
+        {
+            _id: id,
+        },
+        {
+            $set: updates,
+        },
+        {
+            new: true,
+            runValidators: true,
+        }
+    )
+
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: "User not found!"
+        });
+    }
+
+
+    return res.status(200).json({
+        user,
+        success: true,
+        message: "User updated successfully!"
+    });
+
+});
+
 module.exports = {
     getProfile,
+    editProfile,
 };
--- api/routes/profile_routes.js
+++ api/routes/profile_routes.js
@@ -1,11 +1,12 @@
 const app = require('../app');
 const express = require('express');
 const router = express.Router();
-const { getProfile } = require('../controllers/profile_controller');
+const { getProfile, editProfile } = require('../controllers/profile_controller');
 const authorization = require('../middlewares/authorization');
 
 
 router.get('/profile', authorization, getProfile);
+router.patch('/edit-profile', authorization, editProfile);
 
 
 
